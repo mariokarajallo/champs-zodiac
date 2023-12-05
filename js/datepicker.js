dccccfeec68fb27af8de4bcbe617fd7da7708a64
Mario Karajallo <mariokarajallo92@gmail.com>
2023-12-04 21:24:35 -0300

    feat: add page fecha_nacimiento

---
+++ js/datepicker.js
@@ -0,0 +1,10 @@
+// Inicializar el selector de fecha
+$(document).ready(function () {
+  $(".datepicker").datepicker({
+    language: "es",
+    format: "dd/mm/yyyy",
+    clearBtn: true,
+    orientation: "bottom auto",
+    todayHighlight: true,
+  });
+});
